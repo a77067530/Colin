4fcc7c3fc28b344866dc5f20fbc87f1429a3fca6
a77067530 <73977742+a77067530@users.noreply.github.com>
2021-01-15 13:49:23 +0800

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,29 @@
+//#include<iostream>
+//using namespace std;	//打开C++标准库的头文件
+//
+//int main()
+//{
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,0};
+//	/*for(int i=0;i<10;i++)
+//	{
+//		cout<<arr[i]<<endl;
+//	}*/
+//
+//	//for(int nVal:arr)	//自动变量arr这个数组，把arr[i]值赋给nVal
+//	//{
+//	//	cout<<nVal<<" ";
+//	//}
+//
+//	//for(auto nVal:arr)	//auto 类型 自动识别
+//	//{
+//	//	cout<<nVal<<" ";
+//	//}
+//
+//	bool bflag = false;		//布尔类型 值有两个 true false bool是一个字节
+//
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ 源.cpp
@@ -0,0 +1,20 @@
+//#include <iostream>
+//using namespace std;	//打开C++标准库的头文件
+//
+//int main()
+//{
+//	int nVal = 100;
+//	char c = 'a';
+//
+//	cout<<nVal<<" "<<c<<" "<<"哈哈"<<endl;	//endl 换行
+//
+//	cin>>nVal>>c;
+//
+//	cout<<nVal<<" "<<c<<endl;
+//	/*cout<<c;
+//	cout<<nVal;
+//	cout<<"hello C++";*/
+//
+//
+//	return 0;
+//}
